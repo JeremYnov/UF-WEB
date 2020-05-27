f840797617b44c4ce9ae84690f4ecd395adf1ba5
Jérémy <jeremy.richard@ynov.com>
2020-05-27 16:59:47 +0200

    adding restaurant card component

---
+++ vue/src/components/restaurant-card.vue
@@ -0,0 +1,72 @@
+<template>
+  <section class="our-selection">
+    <h2 class="section-title">Notre selection</h2>
+
+    <div class="grid33-33-33">
+      <div class="project">
+        <a href="#">
+          <div class="project-name">
+            <h3>Nom du restaurant</h3>
+            <p>Adresse du restaurant</p>
+          </div>
+          <div class="container">
+            <img src="assets/img/pygame-logo.png" alt class="image" />
+            <!-- <div class="overlay"></div>
+            <div class="link-icon">
+              <i class="fas fa-link"></i>
+            </div>-->
+          </div>
+          <div class="project-description">
+            <i class="fas fa-tags"></i>
+            <p>Catégories du restaurant</p>
+          </div>
+        </a>
+      </div>
+
+      <div class="project 1">
+        <a href="#">
+          <div class="project-name">
+            <h3>Altered Py</h3>
+            <p>Réalisation d'un jeu vidéo en 2D</p>
+          </div>
+          <div class="container">
+            <img src="assets/img/pygame-logo.png" alt class="image" />
+            <!-- <div class="overlay"></div>
+            <div class="link-icon">
+              <i class="fas fa-link"></i>
+            </div>-->
+          </div>
+          <div class="project-description">
+            <i class="fas fa-tags"></i>
+            <p>Python / Pygame</p>
+          </div>
+        </a>
+      </div>
+
+      <div class="project 1">
+        <a href="#">
+          <div class="project-name">
+            <h3>Altered Py</h3>
+            <p>Réalisation d'un jeu vidéo en 2D</p>
+          </div>
+          <div class="container">
+            <img src="assets/img/pygame-logo.png" alt class="image" />
+            <!-- <div class="overlay"></div>
+            <div class="link-icon">
+              <i class="fas fa-link"></i>
+            </div>-->
+          </div>
+          <div class="project-description">
+            <i class="fas fa-tags"></i>
+            <p>Python / Pygame</p>
+          </div>
+        </a>
+      </div>
+    </div>
+  </section>
+</template>
+
+
+<style lang="scss">
+@import "../assets/scss/main.scss";
+</style>
--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,44 +1,8 @@
 <template>
   <div class="home-container">
       <Hero />
-    <section class="our-selection">
-      <h2 class="section-title">Notre selection</h2>
-      <div class="grid33-33-33">
-        <div class="presentation-card-container">
-          <div class="card-image">
-            <img
-              src="https://cdn-food.konbini.com/images/files/2017/06/capture-decran-2017-06-15-a-11.43.30.jpg?webp="
-              alt
-            />
-          </div>
-          <div class="card-title"></div>
-          <div class="card-note"></div>
-          <div class="card-category"></div>
-        </div>
-        <div class="presentation-card-container">
-          <div class="card-image">
-            <img
-              src="https://cdn-food.konbini.com/images/files/2017/06/capture-decran-2017-06-15-a-11.43.30.jpg?webp="
-              alt
-            />
-          </div>
-          <div class="card-title"></div>
-          <div class="card-note"></div>
-          <div class="card-category"></div>
-        </div>
-        <div class="presentation-card-container">
-          <div class="card-image">
-            <img
-              src="https://cdn-food.konbini.com/images/files/2017/06/capture-decran-2017-06-15-a-11.43.30.jpg?webp="
-              alt
-            />
-          </div>
-          <div class="card-title"></div>
-          <div class="card-note"></div>
-          <div class="card-category"></div>
-        </div>
-      </div>
-    </section>
+    
+    <RestaurantCard />
   </div>
 </template>
 
@@ -46,12 +10,14 @@
 // @ is an alias to /src
 // import HelloWorld from "@/components/HelloWorld.vue";
 import Hero from "@/components/hero/hero.vue";
+import RestaurantCard from "@/components/restaurant-card.vue";
 
 export default {
   name: "Home",
   components: {
     // HelloWorld,
-    Hero
+    Hero,
+    RestaurantCard,
   }
 };
 </script>
