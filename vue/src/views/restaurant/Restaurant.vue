f29efa29212c52e6e75e94a5a79a196a651e0deb
David Teixeira <david.teixeira31@gmail.com>
2020-05-28 17:59:02 +0200

    Request axios plate of restaurant template vue

--- vue/src/views/restaurant/Restaurant.vue
+++ vue/src/views/restaurant/Restaurant.vue
@@ -1,17 +1,47 @@
 <template>
   <div class="restaurant">
-      <h1>{{id}}</h1>
+    <h1>{{restaurant.name}}</h1>
   </div>
 </template>
 <script>
+import axios from "axios";
+
 export default {
   data: function() {
     return {
-      id: 0
+      restaurant: null,
+      plates: null
     };
   },
   mounted: function() {
-      this.id = this.$route.params.id
+    this.getRestaurantPlate();
+  },
+  methods: {
+    async getRestaurantPlate() {
+      const response = await axios
+        .get("/api/restaurant/" + this.$route.params.id + "/plates")
+        .then(function(response) {
+          return response;
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+      this.restaurant = {
+        id: response.data.results.id,
+        name: response.data.results.name,
+        category: response.data.results.category,
+        logo: {
+          url: response.data.results.logo.url,
+          name: response.data.results.logo.name
+        },
+        address: response.data.results.address,
+        mail: response.data.results.mail,
+        creation: response.data.results.creation,
+        selection: response.data.results.selection
+      };
+
+      this.plates = response.data.results.plates;
+    }
   }
 };
 </script>
