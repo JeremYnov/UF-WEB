1245d2acbe25c5d17ec3bf144ab4b1ff7de8ecc4
Jérémy <jeremy.richard@ynov.com>
2020-05-29 14:40:26 +0200

    added style on restaurant page

--- vue/src/views/restaurant/Restaurant.vue
+++ vue/src/views/restaurant/Restaurant.vue
@@ -5,11 +5,34 @@
       v-bind:style="{ 'background-image': 'linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)),'+'url(' + restaurant.logo.url + ')' }"
     >
       <div class="restaurant-hero-content">
-        <h1>{{restaurant.name}}</h1>
-        <p>{{restaurant.address}}</p>
-        <p>{{restaurant.category}}</p>
+        <h1 class>{{restaurant.name}}</h1>
+        <p class="restaurant-address">{{restaurant.address}}</p>
+        <div class="restaurant-category">
+          <p>{{restaurant.category}}</p>
+        </div>
       </div>
     </div>
+    <section class="menu">
+      <div class="grid33-33-33">
+        <div class="plates" v-for="plate in plates" :key="plate.id">
+          {{plate}}
+          <div class="plate-container">
+            <div class="plate-image">
+              <img v-bind:src="plate.picture.url" alt />
+            </div>
+            <div class="plate-informations">
+              <h2>{{plate.name}}</h2>
+              <p>{{plate.content}}</p>
+              <p>{{plate.unitPrice}}</p>
+            </div>
+          </div>
+          <!-- <p v-if="plate.type == 'Menu'">Menu : {{plate.name}}</p>
+          <p v-if="plate.type == 'Plate'">Plat : {{plate.name}}</p>
+          <p v-if="plate.type == 'Dessert'">Dessert : {{plate.name}}</p>
+          <p v-if="plate.type == 'Boisson'">Boisson : {{plate.name}}</p> -->
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 <script>
