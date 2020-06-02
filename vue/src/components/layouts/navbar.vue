e602a9905194b9a94115eaa6b7e3dcfb0722093e
Jérémy <jeremy.richard@ynov.com>
2020-06-02 15:38:24 +0200

    added correction for navbar

--- vue/src/components/layouts/navbar.vue
+++ vue/src/components/layouts/navbar.vue
@@ -1,28 +1,35 @@
 <template>
   <div>
-    <div class="nav-responsive" v-bind:class="{ 'nav-transition' : isBurgerActive}">
-      <ul class="nav-list">
-        <li class="nav-item">
+    <div
+      class="nav-responsive"
+      v-bind:class="{ 'nav-transition': isBurgerActive }"
+    >
+      <ul class="header-nav-list">
+        <li class="header-nav-item">
           <a href="#">Home</a>
         </li>
 
-        <li class="nav-item">
+        <li class="header-nav-item">
           <a href="#">About</a>
         </li>
 
-        <li class="nav-item">
+        <li class="header-nav-item">
           <a href="#">Blog</a>
         </li>
-        <li class="nav-item">
+        <li class="header-nav-item">
           <a href="#">Inscription</a>
         </li>
-        <li class="nav-item">
+        <li class="header-nav-item">
           <a href="#">Connexion</a>
         </li>
       </ul>
     </div>
 
-    <div class="overlay" v-bind:class="{ 'is-open' : isBurgerActive }" v-on:click="toggle()"></div>
+    <div
+      class="overlay"
+      v-bind:class="{ 'is-open': isBurgerActive }"
+      v-on:click="toggle()"
+    ></div>
 
     <header>
       <div class="header-main">
@@ -33,47 +40,66 @@
         </div>
 
         <nav class="header-nav">
-          <ul class="nav-list">
-            <li class="nav-item">
-              <router-link :to="{ name: 'Restaurants'}">Restaurants</router-link>
+          <ul class="header-nav-list">
+            <li class="header-nav-item">
+              <router-link :to="{ name: 'Restaurants' }"
+                >Restaurants</router-link
+              >
             </li>
 
-            <li class="nav-item middle-item">
+            <li class="header-nav-item middle-item">
               <a href="/about">About</a>
             </li>
 
-            <li class="nav-item">
+            <li class="header-nav-item">
               <a href="#">Blog</a>
             </li>
           </ul>
         </nav>
         <nav class="header-nav">
-          <ul v-if="session" class="nav-list">
-            <li class="nav-item">
-              <router-link :to="{ name: 'UserLogout'}">Deconnexion</router-link>
+          <ul v-if="session" class="header-nav-list">
+            <li class="header-nav-item shopping-cart-item">
+              <img src="../../assets/icons/icons8-panier-80.png" alt="" />
+            </li>
+            <li class="header-nav-item profile-item">
+              <img src="../../assets/icons/icons8-compte-test-80.png" alt="" />
+              <ul class="sub-menu">
+                <li class="header-nav-item">
+                  <router-link :to="{ name: 'UserProfile' }">Profil</router-link>
+                </li>
+                <li class="header-nav-item">
+                  <router-link :to="{ name: 'UserLogout' }"
+                    >Déconnexion</router-link
+                  >
+                </li>
+              </ul>
             </li>
           </ul>
-          <ul v-else class="nav-list">
-            <li class="nav-item left-item">
+          <ul v-else class="header-nav-list">
+            <li class="header-nav-item left-item">
               <a href="#">Inscription</a>
               <ul class="sub-menu">
-                <li class="nav-item">
-                  <router-link :to="{ name: 'UserSignup'}">Client</router-link>
+                <li class="header-nav-item">
+                  <router-link :to="{ name: 'UserSignup' }">Client</router-link>
                 </li>
-                <li class="nav-item">
-                  <router-link :to="{ name: 'RestaurantSignup'}">Restaurant</router-link>
+                <li class="header-nav-item">
+                  <router-link :to="{ name: 'RestaurantSignup' }"
+                    >Restaurant</router-link
+                  >
                 </li>
               </ul>
             </li>
 
-            <li class="nav-item">
+            <li class="header-nav-item">
               <a href="#">Connexion</a>
               <ul class="sub-menu">
-                <li class="nav-item">
-                  <router-link :to="{ name: 'UserLogin'}">Client</router-link>
+                <li class="header-nav-item">
+                  <router-link :to="{ name: 'UserLogin' }">Client</router-link>
                 </li>
-                <li class="nav-item">
-                  <router-link :to="{ name: 'RestaurantLogin'}">Restaurant</router-link>
+                <li class="header-nav-item">
+                  <router-link :to="{ name: 'RestaurantLogin' }"
+                    >Restaurant</router-link
+                  >
                 </li>
               </ul>
             </li>
@@ -81,7 +107,10 @@
         </nav>
 
         <div class="header-nav-wrapper" v-on:click="toggle()">
-          <div class="header-nav-burger" v-bind:class="{ 'is-animate' : isBurgerActive }"></div>
+          <div
+            class="header-nav-burger"
+            v-bind:class="{ 'is-animate': isBurgerActive }"
+          ></div>
         </div>
       </div>
     </header>
@@ -94,14 +123,14 @@ export default {
   data: () => {
     return {
       session: JSON.parse(localStorage.getItem("session")),
-      isBurgerActive: false
+      isBurgerActive: false,
     };
   },
 
   methods: {
     toggle() {
       this.isBurgerActive = !this.isBurgerActive;
-    }
-  }
+    },
+  },
 };
 </script>
